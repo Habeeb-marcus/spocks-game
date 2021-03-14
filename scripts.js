d384d9894b78834e5fd4a3ce04f3f87275a23ab6
Habeeb <h.makusota@outlook.com>
2021-03-14 15:19:04 +0100

    second commit

--- scripts.js
+++ scripts.js
@@ -1,1 +1,32 @@
-console.log('testing');
+const icon = document.querySelector('.far');
+const playerScoreEl = dcoument.getElementById('playerScore');
+const playerChoiceEl = document.getElementById('playerChoice');
+const computerScoreEl = documeny.getElementById('computerScore');
+const computerChoiceEl = document.getElementById('computerChoice');
+const resultText = document.getElementById('resultText');
+
+// const for player icons
+const playerRock = document.getElementById('playerRock');
+const playerPaper = document.getElementById('playerPaper');
+const playerScissors = document.getElementById('playerScissors');
+const playerSpock = document.getElementById('playerSpock');
+ 
+
+const allGamesIcons =document.querySelectorAll('.far');
+
+// const for computer icons
+const computerRock = document.getElementById('computerRock');
+const computerPaper = document.getElementById('computerPaper');
+const computerScissors = document.getElementById('computerScissors');
+const computerSpock = document.getElementById('computerSpock');
+
+const choices = {
+  rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
+  paper: { name: 'Paper', defeats: ['rock', 'spock'] },
+  scissors: { name: 'Scissors', defeats: ['paper', 'lizard'] },
+  lizard: { name: 'Lizard', defeats: ['paper', 'spock'] },
+  spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
+};
+
+// passing player selection value and styling icons
+function select()
